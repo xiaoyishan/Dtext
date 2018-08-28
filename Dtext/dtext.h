d8b1f4984287e23351467a545a7743340cfbe2fa
xiaSang <xiasang@MacBook-Pro.local>
2018-08-28 10:07:39 +0800

    pod文件

---
+++ Dtext/dtext.h
@@ -0,0 +1,13 @@
+//
+//  dtext.h
+//  dtext
+//
+//  Created by xiaSang on 2018/8/27.
+//  Copyright © 2018年 Ys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dtext : NSObject
+
+@end
---
+++ Dtext/dtext.m
@@ -0,0 +1,13 @@
+//
+//  dtext.m
+//  dtext
+//
+//  Created by xiaSang on 2018/8/27.
+//  Copyright © 2018年 Ys. All rights reserved.
+//
+
+#import "dtext.h"
+
+@implementation dtext
+
+@end
